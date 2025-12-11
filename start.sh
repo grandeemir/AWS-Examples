bdef0cc5c881d455f0372c8f552ea3b7688ac050
emircan <emircanozkara10@gmail.com>
2025-12-11 17:48:51 +0300

    ascii art and start.sh file added

---
+++ start.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Display ASCII Art
+cat << "AWS"
+
+  ⣠⣤⣤⣤⣤⡀⠀⢠⣤⠀⠀⠀⢠⣤⡄⠀⠀⢀⣤⡄⠀⣠⣤⣶⣤⣄⠀⠀
+⠀⠈⠉⠉⠀⠈⢹⣿⡆⠈⣿⣇⠀⠀⣾⣿⣇⠀⠀⣼⡿⠁⢸⣿⠉⠀⠈⠉⠁⠀
+⠀⠀⣠⣤⣶⣦⣴⣿⡇⠀⠹⣿⠀⢰⡿⠈⣿⡄⢀⣿⠃⠀⠘⠻⣷⣶⣤⣄⠀⠀
+⠀⢸⣿⠁⠀⠀⢠⣿⡇⠀⠀⢿⣇⣼⡇⠀⢹⣧⣼⡟⠀⠀⠀⠀⠀⠀⠉⣿⡇⠀
+⠀⠘⢿⣶⣶⡶⠟⢿⠷⠀⠀⠘⣿⡿⠀⠀⠘⣿⡿⠁⠀⠀⠲⣶⣶⣶⣶⠿⠃⠀
+
+⠢⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠚⠛⠛⢿⣷
+⠀⠈⠙⠿⣶⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣴⠾⠀⣸⡏
+⠀⠀⠀⠀⠈⠉⠛⠿⢿⣿⣷⣶⣶⣶⣶⣶⣶⣶⣶⣶⣿⠿⠿⠛⠉⠀⠀⠠⠋⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠙⠛⠛⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+AWS
+
+echo ""
+echo "📚 Available Scripts:"
+echo ""
+echo "  📂 S3 Management Scripts:"
+echo "     • ./s3/bash-scripts/create-bucket    - Create an S3 bucket"
+echo "     • ./s3/bash-scripts/delete-bucket    - Delete an S3 bucket"
+echo "     • ./s3/bash-scripts/list-bucket      - List S3 buckets"
+echo "     • ./s3/bash-scripts/list-object      - List objects in a bucket"
+echo "     • ./s3/bash-scripts/put-object       - Upload objects to bucket"
+echo "     • ./s3/bash-scripts/sync             - Sync files with S3"
+echo ""
+echo "🔗 For more information, check the README.md file"
+echo ""
+
